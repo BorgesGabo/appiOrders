a368ea5404aec77a99024fa342a40a47830dc038
Herbert V <herbertvargas@gmail.com>
2017-07-26 11:44:57 -0500

    filter the summary chart per supplier

--- controler/solution.py
+++ controler/solution.py
@@ -375,3 +375,146 @@ def processPo():
     msg = T("%s registros encontrados" % count)
     queryGen(query)
     return dict(form=form, msg=msg, results=results)
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def loadFormPoDetailQuery():
+    # this function uploads and handles the form based on db.po_detail's table also uploads a query which select in reverse order all data in db.po_detail table
+    ordenes = db(db.po_detail.id > 0).select(orderby=~db.po_detail.po_id)
+    form = SQLFORM(db.po_detail, buttons=[TAG.button('guardar', _type="submit"),
+                                          TAG.button('actualizar listado', _type="button",
+                                                     _onClick="parent.location='%s' " % URL(loadFormPoDetailQuery))])
+
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(ordenes=ordenes, form=form)
+
+
+def loadFormPoQuery():
+    # this function uploads and handles the form based on db.po's table also uploads a query which select in reverse order all data in db.po's table
+    ordenes = db(db.po.id > 0).select(orderby=~db.po.id)
+    form = SQLFORM(db.po, buttons=[TAG.button('guardar', _type="submit"),
+                                   TAG.button('actualizar listado', _type="button",
+                                              _onClick="parent.location='%s'" % URL(loadFormPoQuery)),
+                                   TAG.button('siguiente', _type="button",
+                                              _onClick=" parent.location='%s'" % URL(loadFormPoDetailQuery))])
+    if form.process().accepted:
+        response.flash = 'order accepted'
+    elif form.errors:
+        response.flash = 'check the data inserted'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(ordenes=ordenes, form=form)
+
+
+def loadGridEditCustomer():
+    # This function generates a grid form based on db.product's table
+    grid = SQLFORM.grid(db.customer, user_signature=False)
+    return locals()
+
+
+def loadGridEditProduct():
+    # This function generates a grid form based on db.product's table
+    grid = SQLFORM.grid(db.product, user_signature=False)
+    return locals()
+
+
+def loadGridEditPoDetail():
+    # This function generates a grid form based on db.product's table
+    grid = SQLFORM.grid(db.po_detail, user_signature=False)
+    return locals()
+
+
+def loadFormProduct():
+    # This function generates a form based on db.product's table
+    form = SQLFORM(db.product, buttons=[TAG.button('guardar', _type="submit")])
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(form=form)
+
+
+def loadFormPoDetail():
+    # This function generates a form db.po_detail's form
+    form = SQLFORM(db.po_detail)
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(form=form)
+
+
+def loadFormPo():
+    # This function generates a form based on db.po's table
+    form = SQLFORM(db.po, buttons=[TAG.button('save', _type="submit"), TAG.button('next', _type="button",
+                                                                                  _onClick="parent.location='%s' " % URL(
+                                                                                      loadFormPoDetail))])
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(form=form)
+
+
+def loadFormCustomer():
+    # This function generates a form based on db.customer's table
+    form = SQLFORM(db.customer, buttons=[TAG.button('guardar', _type="submit"), TAG.button('siguiente', _type="button",
+                                                                                           _onClick="parent.location='%s' " % URL(
+                                                                                               loadFormPoQuery))])
+
+    if form.process().accepted:
+        response.flash = 'form accepted'
+    elif form.errors:
+        response.flash = 'form has errors'
+    else:
+        response.flash = 'please fill out the form'
+    return dict(form=form)
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
